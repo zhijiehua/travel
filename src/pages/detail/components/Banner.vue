c82dea377472580e0ae144674cc6f35d75a5d94c
unknown <2010225130@qq.com>
2019-05-29 09:25:32 +0800

    add transition

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -15,21 +15,25 @@
         </div>
     </div>
   </div>
-  <common-gallary
-    :imgs='bannerImgs'
-    v-show="showGallery"
-    @close="bannerClick"
-  >
-  </common-gallary>
+  <fade-animation>
+    <common-gallary
+      :imgs='bannerImgs'
+      v-show="showGallery"
+      @close="bannerClick"
+    >
+    </common-gallary>
+  </fade-animation>
 </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/gallary/FadeAnimaion'
 export default {
   name: 'DetailBanner',
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   data () {
     return {
